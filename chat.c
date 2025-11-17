9d6d1cc1aa44db30d37849c8f3922f3fb8d6fa7b
Dhrimanti <dhritimancg@gmail.com>
2025-11-17 19:59:23 +0530

    Fixing errors

--- chat.c
+++ chat.c
@@ -2,9 +2,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main(){
     int sock_fd=socket(AF_INET,SOCK_STREAM,0);
-    
+
+    struct sockaddr_in server_addr={
+        .sin_family=AF_INET,
+        .sin_port=htons(9999),
+        .sin_addr.s_addr=inet_addr("127.0.0.1")
+    };
+
 }
