f12e148580f4c6947fecb79dbce024b597fe0705
Dhrimanti <dhritimancg@gmail.com>
2025-11-18 10:07:00 +0530

    hey

--- chat.c
+++ chat.c
@@ -6,12 +6,15 @@
 #include <arpa/inet.h>
 
 int main(){
-    int sock_fd=socket(AF_INET,SOCK_STREAM,0);
+    int server_fd=socket(AF_INET,SOCK_STREAM,0);
 
     struct sockaddr_in server_addr={
         .sin_family=AF_INET,
         .sin_port=htons(9999),
         .sin_addr.s_addr=inet_addr("127.0.0.1")
     };
+    bind(server_fd,(struct sockaddr*)&server_addr,sizeof(server_addr));
+    
+
 
 }
